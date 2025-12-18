0f1038b4b84a0f60d8732799bb12a22c03224912
CarlosMS12 <carlosmagallanes207@gmail.com>
2025-12-17 20:17:38 -0500

    fix image

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,6 @@
 import {createClient} from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://udlzzxvyifesjrergedu.supabase.co';
-const supabaseAnonKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkbHp6eHZ5aWZlc2pyZXJnZWR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4NzAwODIsImV4cCI6MjA2ODQ0NjA4Mn0.d2yoAMTiSm7bSxnynUWM5YAnn0aJfLIC1iisJy3KMs4';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
